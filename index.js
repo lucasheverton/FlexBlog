6068651c375ca9b0e535142e285a0e613ad6cc71
Lucas <lucas.santos@wecontent.com.br>
2021-08-16 01:17:55 -0300

    add new svg, add new button scroll to top

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+/* Tratamento de Datas */
 var data = new Date();
 var ano = data.getFullYear();
 
 document.querySelector(".hora").innerText = `${ano}`;
+
+/* Scroll top */
+var scrollToTopBtn = document.getElementById("scrollTop");
+var rootScroll = document.documentElement;
+
+function scrollToTop() {
+  rootScroll.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
